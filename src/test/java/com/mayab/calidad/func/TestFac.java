52e6b04b4908015a33ab40a96ec0f9ecc790c99b
Leonel Sosa <ernesto.leonel.vera.sosa@gmail.com>
2020-11-30 18:32:39 -0600

    40

--- src/test/java/com/mayab/calidad/func/TestFac.java
+++ src/test/java/com/mayab/calidad/func/TestFac.java
@@ -18,7 +18,7 @@ public class TestFac {
 
 	  @Before
 	  public void setUp() throws Exception {
-		  System.setProperty("webdriver.chrome.driver", "D:\\negoc\\Descargas\\instaladores\\chromedriver.exe");
+		  System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 	    driver = new ChromeDriver();
 	    baseUrl = "https://www.google.com/";
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
